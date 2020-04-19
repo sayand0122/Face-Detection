7cd00aca37bfabe07e3624047ec47222b17b540b
Sayan Dutta <sayandutta.jan@gmail.com>
2020-04-19 12:18:57 +0530

    Add files via upload

---
+++ Face detection.py
@@ -0,0 +1,17 @@
+import cv2
+
+cap=cv2.VideoCapture(0)
+detect = cv2.CascadeClassifier(r'C:\Users\Sayan\Downloads\FaceDetection-master\haarcascade_frontalface_default.xml')
+
+while True:
+    ret, image=cap.read();
+    gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face=detect.detectMultiScale(image,1.3,5)
+    for(x,y,w,h) in face:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),3)
+    cv2.imshow('Camera',image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
